975439e88aeca8f19911b62089ab6069fe11f488
David Keim <dmk252@cornell.edu>
2023-05-18 11:40:06 -0400

    added model for users

---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+import pkg from 'mongoose';
+const { Schema, model } = pkg;
+
+// defines user schema
+const userSchema = new Schema(
+    {
+      username: { type: String, required: true, unique: true, trim: true },
+      // validates email format
+      email: { type: String, required: true, unique: true, trim: true, match: /.+\@.+\..+/, },
+      // defines list of thought and friend IDs linked to relevant objects
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      }
+    }
+  );
+  
+  const User = model('user', userSchema);
+  
+  module.exports = User;
+  
