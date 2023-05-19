29e7cbb3b616eb0b1eb89276f32425794d12f5b3
David Keim <dmk252@cornell.edu>
2023-05-19 12:24:21 -0400

    added user crud routes

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,10 +1,47 @@
 import { Router } from "express";
+import models from '../models/index.js';
 
 const router = Router();
 
-// /api/tests/
+// /api/test/
 router.get('/test', (req, res) => {
     res.json({ message: "hello from server" });
 });
+
+// /api/users/
+router.get('/users', (req, res) => {
+    models.User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+})
+
+// /api/users/:id
+router.get('/users/:id', (req, res) => {
+    models.User.findOne({ _id: req.params.id })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+})
+
+router.post('/users/', (req, res) => {
+    models.User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+})
+
+router.put('/users/:id', (req, res) => {
+    models.User.findOneAndUpdate({ _id: req.params.id }, { username: req.body.username, email: req.body.email }, { new: true} )
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => res.status(500).json(err));
+})
+
+router.delete('/users/:id',  (req, res) => {
+    models.User.findOneAndDelete({_id: req.params.id})
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => res.status(500).json(err));
+})
 
 export default router;
