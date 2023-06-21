8aa22592e499ee44ed869b18a8d0f042bd3090a6
David Keim <dmk252@cornell.edu>
2023-06-20 21:10:16 -0400

    basic left implementation

--- client/src/pages/LevelOne.jsx
+++ client/src/pages/LevelOne.jsx
@@ -459,9 +459,146 @@ function LevelOne() {
                 setLastXMove(1);
             break;
             case "ArrowLeft":
-                if (min <= current - 15) {
-                    editStatus(current, current - 15, "Selected")
+                // clear  previous selected
+                for (let l = 0; l < cells.length; l++) {
+                    cells[l].status = "None";
                 }
+                if (commandPressed === 1) {
+                    // set up variables for while loop - starting on the current cell to iterate through until the stopping cell is found
+                    let proposedNewCell = current;
+                    let canContinue = true;
+                    let currentContent = cells[current].contents;
+                    let nextContent = cells[(current - noRows < min ? current : current - noRows)].contents;
+                    // if the shift key is held and the user is moving in the same direction as the last move, use the edge Y instead of current
+                    if (event.shiftKey && lastXMove === -1) {
+                        proposedNewCell = edgeX;
+                        currentContent = cells[edgeX].contents;
+                        nextContent = cells[(edgeX - noRows < min ? edgeX : edgeX - noRows)].contents;
+                    }
+                    // continue incrementing forward while not at end of column (modulus 14), and when content rules are met
+                    while (proposedNewCell - noRows >= min && canContinue === true) {
+                        proposedNewCell = proposedNewCell - noRows;
+                        if (currentContent === "") {
+                            // accounts for case with top left/bottom right cell
+                            try {
+                                // if current cell is blank and the next proposed one isn't
+                                if (cells[proposedNewCell - noRows].contents !== "" && cells[proposedNewCell].contents === "") {
+                                    canContinue = false;
+                                    // move on to the next cell (per excel's behavior)
+                                    if (proposedNewCell - noRows >= min && cells[proposedNewCell].contents === "") {
+                                        proposedNewCell = proposedNewCell - noRows;
+                                    }
+                                }
+                            }
+                            catch {
+                                canContinue = false;
+                            }
+                        }
+                        else {
+                            try {
+                                // if current cell isn't blank and if it's the last row of the data before blanks
+                                if (nextContent === "") {
+                                    // behave as in the previous case - go to the next block of data (or the end)
+                                    if (cells[proposedNewCell - noRows].contents !== "") {
+                                        canContinue = false;
+                                        if (proposedNewCell - noRows >= min) {
+                                            proposedNewCell = proposedNewCell - noRows;
+                                        }
+                                    }
+                                }
+                                // otherwise continue until it hits a blank, but stay in the block of data
+                                else if (cells[proposedNewCell - noRows].contents === "") {
+                                    canContinue = false;
+                                }
+                            }
+                            catch {
+                                canContinue = false;
+                            }
+                        }
+                    }
+                    // if shift key is held, multiple cells need to be selected, corresponding to the newYValue and selectMultipleX as dimensions
+                    if (event.shiftKey) {
+                        let newValueX = (proposedNewCell - current) / noRows;
+                        setSelectMultipleX(newValueX);
+                        setEdgeX(proposedNewCell);
+                        // if x width is not zero, loop through x then y
+                        if (newValueX !== 0) {
+                            for (let k = 0; (newValueX >= 0? k < newValueX: k > newValueX); (newValueX >= 0 ? k++ : k--)) {
+                                // if y is zero, fill in the cell here since the inner loop won't be reached
+                                if (selectMultipleY === 0) {
+                                    cells[current + (newValueX >= 0 ? k + 1 : k - 1) * noRows].status = "Selected";
+                                }
+                                for (let m = 0; (selectMultipleY >= 0? m < selectMultipleY: m > selectMultipleY); (selectMultipleY >= 0 ? m++ : m--)) {
+                                    cells[current + (newValueX >= 0 ? k + 1 : k - 1) * noRows + (selectMultipleY >= 0 ? m + 1 : m - 1)].status = "Selected";
+                                }
+                            }
+                        }
+                        else {
+                            // loop through y if x = 0 as the other loop won't be reached
+                            for (let m = 0; (selectMultipleY >= 0? m < selectMultipleY: m > selectMultipleY); (selectMultipleY >= 0 ? m++ : m--)) {
+                                cells[current + (selectMultipleY >= 0 ? m + 1 : m - 1)].status = "Selected";
+                            }
+                        }
+                        editStatus(current, current, "Selected");
+                    }
+                    else {
+                        editStatus(current, proposedNewCell, "Selected");
+                        setEdgeY(proposedNewCell);
+                        setEdgeX(proposedNewCell);
+                        for (let l = 0; l < cells.length; l++) {
+                            if (l !== proposedNewCell) {
+                                cells[l].status = "None";
+                            }
+                        }
+                        setSelectMultipleY(0);
+                        setSelectMultipleX(0);
+                    }
+                }
+                // if command is not pressed, just iterates by one cell
+                else {
+                    if (min <= current - noRows) {
+                        if (event.shiftKey) {
+                            let newValueX = selectMultipleX - 1;
+                            setSelectMultipleX(newValueX);
+                            // if x width is not zero, loop through x then y
+                            if (newValueX !== 0) {
+                                for (let k = 0; (newValueX >= 0? k < newValueX: k > newValueX); (newValueX >= 0 ? k++ : k--)) {
+                                    // if y is zero, fill in the cell here since the inner loop won't be reached
+                                    if (selectMultipleY === 0) {
+                                        cells[current + (newValueX >= 0 ? k + 1 : k - 1) * noRows].status = "Selected";
+                                    }
+                                    for (let m = 0; (selectMultipleY >= 0? m < selectMultipleY: m > selectMultipleY); (selectMultipleY >= 0 ? m++ : m--)) {
+                                        cells[current + (newValueX >= 0 ? k + 1 : k - 1) * noRows + (selectMultipleY >= 0 ? m + 1 : m - 1)].status = "Selected";
+                                    }
+                                }
+                            }
+                            else {
+                                // loop through y if x = 0 as the other loop won't be reached
+                                for (let m = 0; (selectMultipleY >= 0? m < selectMultipleY: m > selectMultipleY); (selectMultipleY >= 0 ? m++ : m--)) {
+                                    cells[current + (selectMultipleY >= 0 ? m + 1 : m - 1)].status = "Selected";
+                                }
+                            }
+                            editStatus(current, current, "Selected");
+                            setEdgeY(current);
+                            setEdgeX(current);
+                        }
+                        else {
+                            let newCurrent = current - noRows;
+                            editStatus(current, newCurrent, "Selected")
+                            setEdgeY(newCurrent);
+                            setEdgeX(newCurrent);
+                            for (let l = 0; l < cells.length; l++) {
+                                if (l !== newCurrent) {
+                                    cells[l].status = "None";
+                                }
+                            }
+                            setSelectMultipleY(0);
+                            setSelectMultipleX(0);
+
+                        }
+                    }
+                }
+                setLastXMove(-1);
             break;
             case "MetaLeft":
                 setCommandPressed(1);
