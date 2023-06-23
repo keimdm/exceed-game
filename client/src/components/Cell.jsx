ec6b918c5aaa9dfd6fe4606e94fe4a2dadd1f7c0
David Keim <dmk252@cornell.edu>
2023-06-23 12:44:48 -0400

    error cell color

--- client/src/components/Cell.jsx
+++ client/src/components/Cell.jsx
@@ -2,13 +2,22 @@ import { GridItem, Text } from '@chakra-ui/react';
 
 function Cell({colStart, colEnd, rowStart, rowEnd, contents, index, status}) {
 
+    let color = "white";
+    if (contents === "#ERR") {
+        color = "red.300";
+    }
+
+    if (status === "Selected") {
+        color = "blue.300";
+    }
+
     return (
         <GridItem
             colStart={colStart}
             colEnd={colEnd}
             rowStart={rowStart}
             rowEnd={rowEnd}
-            bgColor={status === "Selected" ? "blue.300" : "white"}
+            bgColor={color}
             color="black"
             borderColor="gray.400"
             borderWidth="1px"
